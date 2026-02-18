9360f79afce4b0e4c7ac80be064fa768b6547b16
Carlos Andres Beltran Franco <cbeltran@inmov.com>
2026-02-18 16:49:59 -0500

    fix: actualizar URLs del sitio y sitemap a la nueva dirección; mejorar estructura de encabezados en varios componentes

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://carlosandresbf.dev',
+  site: 'https://carlosandresbf.com',
   output: 'server',
 
   adapter: node({
